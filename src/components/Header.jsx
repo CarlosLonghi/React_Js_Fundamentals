71069e5e16cd2b16d85278ebacae799ce1a92b7a
CarlosLonghi <carlos.longhi@digitalone.com.br>
2023-12-01 08:58:56 -0300

    style: style header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
 import styles from './Header.module.css'
 
+import igniteLogo from '../assets/ignite-logo.svg'
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Social Feed</strong>
+      <div className={styles.brand}>
+        <img src={igniteLogo} alt="Logo"/>
+        <strong>Social Feed</strong>
+      </div>
     </header>
   )
 }
