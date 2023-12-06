bcaa58a4abf9af9dde0450d8d05de36181cc0bb6
CarlosLonghi <carlos.longhi@digitalone.com.br>
2023-12-06 17:20:55 -0300

    teste

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export function Header() {
     <header className={styles.header}>
       <div className={styles.brand}>
         <img src={igniteLogo} alt="Logo"/>
-        <strong>Social Feed</strong>
+        <strong>Social Feed Teste</strong>
       </div>
     </header>
   )
